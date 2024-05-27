4d9e0353608e960f745df1f476a6cae9e94c98b5
zhangga <ryanzhangga1991@gmail.com>
2024-05-27 11:50:40 +0800

    data

--- test.py
+++ test.py
@@ -1,25 +1,25 @@
-# import json
-# from eval_retriever.metrics import Metric
-# from eval_retriever.utils import load_json
-# import warnings
+import json
+from eval_retriever.metrics import Metric
+from eval_retriever.utils import load_json
+import warnings
 
-# warnings.filterwarnings(action="ignore")
-# # pred
-# pred_tables = load_json("eval_retriever/preds/pred_tables.json")
-# pred_columns = load_json("eval_retriever/preds/pred_columns.json")
-# # label
-# label_tables = load_json("eval_retriever/data/y_tables.json")
-# label_columns = load_json("eval_retriever/data/y_columns.json")
-# #################
-# # averaged
-# print(Metric.averaged(pred_tables, label_tables))
-# print(Metric.averaged(pred_columns, label_columns))
-# # jaccard
-# print(Metric.jaccard(pred_tables, label_tables))
-# print(Metric.jaccard(pred_columns, label_columns))
-# # hamming
-# print(Metric.hamming(pred_tables, label_tables))
-# print(Metric.hamming(pred_columns, label_columns))
+warnings.filterwarnings(action="ignore")
+# pred
+pred_tables = load_json("eval_retriever/preds/pred_tables.json")
+pred_columns = load_json("eval_retriever/preds/pred_columns.json")
+# label
+label_tables = load_json("eval_retriever/data/y_tables.json")
+label_columns = load_json("eval_retriever/data/y_columns.json")
+#################
+# averaged
+print(Metric.averaged(pred_tables, label_tables))
+print(Metric.averaged(pred_columns, label_columns))
+# jaccard
+print(Metric.jaccard(pred_tables, label_tables))
+print(Metric.jaccard(pred_columns, label_columns))
+# hamming
+print(Metric.hamming(pred_tables, label_tables))
+print(Metric.hamming(pred_columns, label_columns))
 
 
 from eval_retriever.utils import load_llm
